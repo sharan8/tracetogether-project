68a2ba41ee6f30891dcfde38583702162471cd2d
sharan8 <sharan.t@u.nus.edu>
2021-04-22 00:53:38 +0800

    Add hashmap testing code to nbr_discovery.c

--- nbr_discovery.c
+++ nbr_discovery.c
@@ -9,6 +9,8 @@
 #include "defs_and_types.h"
 #include "net/netstack.h"
 #include "random.h"
+#include <string.h>
+#include "hashmap/hashmap.h"
 #ifdef TMOTE_SKY
 #include "powertrace.h"
 #endif
@@ -37,6 +39,66 @@ void populate_permuation_arr();
 /*---------------------------------------------------------------------------*/
 PROCESS(cc2650_nbr_discovery_process, "cc2650 neighbour discovery process");
 AUTOSTART_PROCESSES(&cc2650_nbr_discovery_process);
+/*---------------------------------------------------------------------------*/
+
+// TESTING HASHMAP
+
+struct user {
+    char *name;
+    int age;
+};
+
+int user_compare(const void *a, const void *b, void *udata) {
+    const struct user *ua = a;
+    const struct user *ub = b;
+    return strcmp(ua->name, ub->name);
+}
+
+bool user_iter(const void *item, void *udata) {
+    const struct user *user = item;
+    printf("%s (age=%d)\n", user->name, user->age);
+    return true;
+}
+
+uint64_t user_hash(const void *item, uint64_t seed0, uint64_t seed1) {
+    const struct user *user = item;
+    return hashmap_sip(user->name, strlen(user->name), seed0, seed1);
+}
+
+static void test_hashmap() {
+  // create a new hash map where each item is a `struct user`. The second
+  // argument is the initial capacity. The third and fourth arguments are 
+  // optional seeds that are passed to the following hash function.
+  struct hashmap *map = hashmap_new(sizeof(struct user), 0, 0, 0, 
+                                    user_hash, user_compare, NULL);
+
+  // Here we'll load some users into the hash map. Each set operation
+  // performs a copy of the data that is pointed to in the second argument.
+  hashmap_set(map, &(struct user){ .name="Dale", .age=44 });
+  hashmap_set(map, &(struct user){ .name="Roger", .age=68 });
+  hashmap_set(map, &(struct user){ .name="Jane", .age=47 });
+
+  struct user *user; 
+  
+  printf("\n-- get some users --\n");
+  user = hashmap_get(map, &(struct user){ .name="Jane" });
+  printf("%s age=%d\n", user->name, user->age);
+
+  user = hashmap_get(map, &(struct user){ .name="Roger" });
+  printf("%s age=%d\n", user->name, user->age);
+
+  user = hashmap_get(map, &(struct user){ .name="Dale" });
+  printf("%s age=%d\n", user->name, user->age);
+
+  user = hashmap_get(map, &(struct user){ .name="Tom" });
+  printf("%s\n", user?"exists":"not exists");
+
+  printf("\n-- iterate over all users --\n");
+  hashmap_scan(map, user_iter, NULL);
+
+  hashmap_free(map);
+}
+
 /*---------------------------------------------------------------------------*/
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
@@ -117,6 +179,8 @@ PROCESS_THREAD(cc2650_nbr_discovery_process, ev, data)
   PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 
   PROCESS_BEGIN();
+
+  test_hashmap();
 
   random_init(54222 + node_id);
 
